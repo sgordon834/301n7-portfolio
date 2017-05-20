e5f3aaf06b3ff2ca6051ec4d25fdad6b77f6b818
sgordon834 <sgordon834@gmail.com>
2017-05-20 12:35:47 -0700

    repos repair

--- public/scripts/repo.js
+++ public/scripts/repo.js
@@ -15,22 +15,26 @@
 //
 
 repos.requestRepos = function(callback) {
-    $.get('/github/user')
-      .then(user => {
-        $.get(user.repos_url)
-          .then(repo => {
-            repo.forEach(results => {
-              repos.all.push(results);
-              callback(repos);
-            })
-            // callback(repos);
-        }, error => {
-            console.log(error);
-          });
-      }, error => {
-        console.log(error);
-      });
-  };
+  $.get('/github/user/repos')
+   .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+   .then(callback);
+ };
+  //   $.get('https://api.github.com/user?access_token=' + window.gitToken)
+  //     .then(user => {
+  //       $.get(user.repos_url + '?access_token=' + window.gitToken)
+  //         .then(repo => {
+  //           repo.forEach(results => {
+  //             repos.all.push(results);
+  //             callback(repos);
+  //           })
+  //           // callback(repos);
+  //       }, error => {
+  //           console.log(error);
+  //         });
+  //     }, error => {
+  //       console.log(error);
+  //     });
+  // };
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
